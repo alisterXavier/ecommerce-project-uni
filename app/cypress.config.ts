37408dff48a9a604b50032a18e923656cdc92a9b
Alister Xavier <xavieralister153@gmail.com>
2024-02-18 00:15:14 +0400

    asdasd

--- app/cypress.config.ts
+++ app/cypress.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  // component: {
-  //   devServer: {
-  //     framework: 'next',
-  //     bundler: 'webpack',
-  //   },
-  //   supportFile: './cypress/support/component.{js,jsx,ts,tsx}',
-
-  // },
   e2e: {
     setupNodeEvents(on, config) {},
-    // supportFile: './cypress/support/e2e.{js,jsx,ts,tsx}',
   },
 });
--- app/cypress/support/e2e.ts
+++ app/cypress/support/e2e.ts
@@ -1,5 +1,5 @@
 // ***********************************************************
-// This example support/component.ts is processed and
+// This example support/e2e.ts is processed and
 // loaded automatically before your test files.
 //
 // This is a great place to put global configuration and
@@ -14,27 +14,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import { createTheme } from '@mantine/core';
-import { MemoryRouterProps } from 'react-router-dom';
-import { MountOptions, MountReturn, mount } from 'cypress/react18';
-import './commands';
+import './commands'
 
-// Augment the Cypress namespace to include type definitions for
-// your custom command.
-// Alternatively, can be defined in cypress/support/component.d.ts
-// with a <reference path="./component" /> at the top of your spec.
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      mount(
-        component: React.ReactNode,
-        options?: MountOptions & { routerProps?: MemoryRouterProps }
-      ): Cypress.Chainable<MountReturn>;
-    }
-  }
-}
-const theme = createTheme({});
-
-Cypress.Commands.add('mount', (component, options = {}) => {
-  return mount(component, options);
-});
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
